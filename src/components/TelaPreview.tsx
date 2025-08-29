bf50c9f33ded773b494d2af5a174f28c348e295a
Biancaprs <bianca.prssantos@gmail.com>
2025-08-29 18:59:43 -0400

    cria componente TelaPreview e sua estrutura

--- src/components/TelaPreview.tsx
+++ src/components/TelaPreview.tsx
@@ -2,8 +2,12 @@ export default TelaPreview;
 
 function TelaPreview() {
   return (
-    <>
-      <h2>Preview do Currículo</h2>
-    </>
+    <div>
+      <div>
+        <h2>Preview do Currículo</h2>
+        <p>Visualização em tempo real das suas informações</p>
+      </div>
+      <div>PreviewCard.tsx</div>
+    </div>
   );
 }
