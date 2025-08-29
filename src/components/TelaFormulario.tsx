79a9f14d5eb22ef9b8a97a9cf1d108c4743ff9ff
Biancaprs <bianca.prssantos@gmail.com>
2025-08-29 04:48:02 -0400

    cria componente TelaFormulario e sua estrutura

--- src/components/TelaFormulario.tsx
+++ src/components/TelaFormulario.tsx
@@ -7,6 +7,24 @@ function TelaFormulario() {
         <h1>Gerador de Currículo Inteligente</h1>
         <p>Crie seu currículo profissional com preview em tempo real</p>
       </div>
+      <div className="flex justify-center">
+        <button className="border border-black">Exportar PDF</button>
+        <p className="text-center">
+          Preencha os campos obrigatórios para habilitar a exportação
+        </p>
+      </div>
+      <div>
+        <h2>DadosCard.tsx</h2>
+      </div>
+      <div>
+        <h2>HabilidadesCard.tsx</h2>
+      </div>
+      <div>
+        <h2>ExperiênciasCard.tsx</h2>
+      </div>
+      <div>
+        <h2>FormaçãoCard.tsx</h2>
+      </div>
     </>
   );
 }
